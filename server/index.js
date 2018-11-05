a14ddc28aa78fd94a4eae07cdd918af3bf547e83
shirley <hu.shirleyj@gmail.com>
2018-11-05 13:07:05 -0500

    Scrape title

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const axios = require('axios');
+const cheerio = require('cheerio');
 require('dotenv').config(); 
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname + '/../client/dist')));
+
+
+  axios.get('https://www.buildmyowncabin.com/nec/nec2014_chap9_table4.html')
+  .then(({data}) => {
+    let $ = cheerio.load(data);
+    let conduits = []
+    $('tr').children().each((i, ele) => {
+      if ($(ele).siblings().length === 0)
+      console.log($(ele).text())
+    })
+  })
+  .catch(err => console.log(err))
+
 
 app.listen(8080, function() {
   console.log('listening on port 8080!');
